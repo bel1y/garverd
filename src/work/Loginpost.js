fda192acf52aa64d6876215645aa386469fd218f
bel1y <132271045+bel1y@users.noreply.github.com>
2023-07-29 17:09:36 +0500

    SADZXVD
    VSDXSD

--- src/work/Loginpost.js
+++ src/work/Loginpost.js
@@ -1,38 +1,91 @@
-import React, {useEffect, useState} from 'react'
+import React, {Component} from 'react'
 import './loginpost.css'
 import axios from 'axios'
-
-export default function Podpis1() {
-    const[state,setState]=useState(sessionStorage.getItem('token'))
-    function postModal(){
-        var data= new FormData()
-
-        data.append("email", document.querySelector('#email').value)
-        data.append("user_password", document.querySelector('#pass').value)
-        data.append("username", document.querySelector('#email').value)
+import {FiEdit} from 'react-icons/fi'
+import {MdDeleteOutline} from 'react-icons/md'
 
 
+export default class App extends Component {
+    state={
+        data:[],
+        login:sessionStorage.getItem('token'),
+        deleteid:{},
+}
 
-        axios.post("https://markaz-yhlk.onrender.com/auth/login", data).then(res=>{
-            console.log(res.data);
-            sessionStorage.setItem('token', res.data.access)
-            window.location.reload()
-        }).catch(err=>{
-            alert("error")
-        })
+  postModal=()=>{
+    var data= new FormData()
+
+    data.append("email", document.querySelector('#email').value)
+    data.append("user_password", document.querySelector('#pass').value)
+    data.append("username", document.querySelector('#email').value)
+
+
+
+    axios.post("https://markaz-yhlk.onrender.com/auth/login", data).then(res=>{
+        console.log(res.data);
+        sessionStorage.setItem('token', res.data.access)
+        window.location.reload()
+    }).catch(err=>{
+        alert("error")
+    })
+}
+
+getModal(){
+    console.log(sessionStorage.getItem('token'));
+    axios.get('https://markaz-yhlk.onrender.com/auth/users', {headers:{Authorization: `Bearer ${sessionStorage.getItem('token')}`}})
+    .then(res=>{
+        this.setState({data:res.data})
+    }).catch(err=>{
+        alert('tupoy')
+    })
+
+}
+
+componentDidMount(){
+    if (this.state.login) {
+        this.getModal()
     }
-    
+}
 
 
-  return (
- <div className='from'> 
+
+
+render() {
+    return (
+      <div>
+        {this.state.login?(<div>
+            {this.state.data.map(item=>{
+            return( <table>
+                <tr>
+                    <th>Id</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                    
+                </tr>
+                <tr>
+                    <td>{item.user_id}</td>
+                    <td>{item.username}</td>
+                    <td>{item.email}</td>
+                    <td><FiEdit/> Edit</td>
+                    <td><MdDeleteOutline/> Delete</td>
+                </tr>
+            </table>)
+            
+            })}
+            
+        </div>):(
+       <div className='from'> 
         <form action="">
             <label htmlFor=""></label><br />
             <input placeholder='email or username' type="text" id='email'/><br />
             <label htmlFor=""></label><br />
             <input type="password" placeholder='password'  name="" id="pass" /><br />
-            <button onClick={()=>postModal()} type='button'>Click me</button>
+            <button onClick={()=>{this.postModal()}} type='button'>Click me</button>
         </form>
- </div>
-  )
+ </div>)}
+
+  
+      </div>
+    )
+  }
 }
